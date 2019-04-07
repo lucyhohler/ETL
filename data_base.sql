e1fddeaa1f45997ae71e2c53ce58563e1b7c4344
Lucy Hohler <lucyhohler@gmail.com>
2019-04-07 15:33:33 -0500

    added DB constraint

--- data_base.sql
+++ data_base.sql
@@ -1,9 +1,9 @@
 -- Create and use world_rate_db
-CREATE DATABASE world_rate_db;
+CREATE DATABASE IF NOT EXISTS world_rate_db;
 USE world_rate_db;
 
 -- Create tables for raw data to be loaded into
-CREATE TABLE countries (
+CREATE TABLE IF NOT EXISTS countries (
   code varchar(3) PRIMARY KEY,
   name TEXT,
   official_name TEXT,
@@ -13,11 +13,12 @@ CREATE TABLE countries (
   source TEXT
 );
 
-CREATE TABLE unemployment_rates (
+CREATE TABLE IF NOT EXISTS unemployment_rates (
   id INT PRIMARY KEY auto_increment not null,
   country_code varchar(3),
   year INT,
   rate decimal(11,8)
 );
 
-
+ALTER TABLE unemployment_rates 
+ADD CONSTRAINT uc_unemployment_rates UNIQUE (country_code, year);
